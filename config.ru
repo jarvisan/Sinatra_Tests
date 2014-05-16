2aff2af1e2735d2b16bf0523d85160e5bade76f6
Andrew Jarvis <andrew.jarvis@Dustins-Mac-mini.local>
2014-05-16 17:06:31 -0400

    added config.ru

---
+++ config.ru
@@ -0,0 +1,5 @@
+require 'sinatra'
+require './stest'
+
+run Sinatra::Application
+
--- stest.rb
+++ stest.rb
@@ -1,18 +1,21 @@
-require 'sinatra'
 require 'test_gem'
 
-get '/FizzBuzz/' do
-	'Enter a Number after FizzBuzz and then a format(plain, html or json), ex: FizzBuzz/45/json'
+get '/' do
+	'Enter a Number after FizzBuzz and then a format(plain, html or json), ex: /45/json'
 end
 
-get '/FizzBuzz/:num/json' do
+get '/:num/json' do
 	main(params[:num], 'json')
 end
 
-get '/FizzBuzz/:num/plain' do
+get '/:num/plain' do
 	main(params[:num], 'plain')
 end
 
-get '/FizzBuzz/:num/html' do
+get '/:num/html' do
 	main(params[:num], 'html')
+end
+
+get '/:num/*' do
+	redirect "/"
 end
