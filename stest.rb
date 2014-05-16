bacc2a0047b2b7f89f2896a4162fcccf826b1548
Andrew Jarvis <andrew.jarvis@Dustins-Mac-mini.local>
2014-05-16 13:42:53 -0400

    added paths for different formats

--- stest.rb
+++ stest.rb
@@ -2,9 +2,17 @@ require 'sinatra'
 require 'test_gem'
 
 get '/FizzBuzz/' do
-	'Enter Numbers after Fizz Buzz, ex: FizzBuzz/45'
+	'Enter Numbers after Fizz Buzz and then a format, ex: FizzBuzz/45/json'
 end
 
-get '/FizzBuzz/:num' do
+get '/FizzBuzz/:num/json' do
 	main(params[:num])
+end
+
+get '/FizzBuzz/:num/plain' do
+	'not finished'
+end
+
+get '/FizzBuzz/:num/html' do
+	'not finished'
 end
