9d7a72975b69cf7e08234678c4370d92ea2ebe5d
Andrew Jarvis <andrew.jarvis@Dustins-Mac-mini.local>
2014-05-16 15:02:41 -0400

    reworded instructions

--- stest.rb
+++ stest.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'test_gem'
 
 get '/FizzBuzz/' do
-	'Enter Numbers after Fizz Buzz and then a format(plain, html or json), ex: FizzBuzz/45/json'
+	'Enter A Number after FizzBuzz and then a format(plain, html or json), ex: FizzBuzz/45/json'
 end
 
 get '/FizzBuzz/:num/json' do
@@ -15,8 +15,4 @@ end
 
 get '/FizzBuzz/:num/html' do
 	main(params[:num], 'html')
-end
-
-get 'FizzBuzz/:num/*' do
-	main(params[:num], 'plain')
 end
