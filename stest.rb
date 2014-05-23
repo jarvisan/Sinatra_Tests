2aebc78ca4ed8d7ce84bbc95200b714c272d942a
Andrew Jarvis <jarvis1990@gmail.com>
2014-05-23 13:02:03 -0400

    Cleaned routes

--- stest.rb
+++ stest.rb
@@ -7,19 +7,3 @@ end
 post '/FizzBuzz' do
 	main(params[:num], params[:formatOpt])
 end
-
-get '/:num/json' do
-	main(params[:num], 'json')
-end
-
-get '/:num/plain' do
-	main(params[:num], 'plain')
-end
-
-get '/:num/html' do
-	main(params[:num], 'html')
-end
-
-get '/:num/*' do
-	redirect "/"
-end
